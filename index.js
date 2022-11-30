9e4cedcb0644ad7072f1500c2aa5f17909181d9a
Dungyy <erickmunoz13@gmail.com>
2022-11-29 18:03:09 -0600

    Api Cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const request = require("request-promise");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config()
+require("dotenv").config();
 
 const apiKey = process.env.API_KEY;
 
@@ -12,8 +12,26 @@ const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
 app.get("/", (req, res) => {
   res.send("Welcome to Dungy's Amazon Scraper API");
+});
+
+//Get Search Results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  try {
+    const response = await request(
+      `${baseURL}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 //Get Product Details
@@ -31,41 +49,28 @@ app.get("/products/:productId", async (req, res) => {
 
 //Get Product Reviews
 app.get("/products/:productId/reviews", async (req, res) => {
-    const { productId } = req.params;
-    try {
-      const response = await request(
-        `${baseURL}&url=https://www.amazon.com/product-reviews/${productId}`
-      );
-      res.json(JSON.parse(response));
-    } catch (error) {
-      res.json(error);
-    }
-  });
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `${baseURL}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 //Get Product offers
 app.get("/products/:productId/offers", async (req, res) => {
-    const { productId } = req.params;
-    try {
-      const response = await request(
-        `${baseURL}&url=https://www.amazon.com/gp/offer-listing/${productId}`
-      );
-      res.json(JSON.parse(response));
-    } catch (error) {
-      res.json(error);
-    }
-  });
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `${baseURL}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-//Get Search Results
-app.get("/search/:searchQuery", async (req, res) => {
-    const { searchQuery } = req.params;
-    try {
-      const response = await request(
-        `${baseURL}&url=https://www.amazon.com/s?k=${searchQuery}`
-      );
-      res.json(JSON.parse(response));
-    } catch (error) {
-      res.json(error);
-    }
-  });  
-  
 app.listen(PORT, () => console.log(`SERVER ON PORT: ${PORT}`));
